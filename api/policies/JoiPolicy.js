f5d099efbf2a74061a45a900d6404a58b53125cc
Nishchit14 <dnishchit@gmail.com>
2019-12-26 10:20:56 +0530

    => allowing config option from validators file for byPassError and => returning detailed validationErrors in req.validationErrors key

--- api/policies/JoiPolicy.js
+++ api/policies/JoiPolicy.js
@@ -28,6 +28,7 @@ module.exports = class JoiPolicy extends Policy {
       let { handler } = routes.find(r=>r.path==req.route.path && r.method==reqMethod)
       let [ controller, method ] = handler.split('.')
       let validation = validators[controller][method]
+      let config = validators.config || {}
       if(!validation) throw new Error('please provide validation in config/validators.js')
       let [module, schema] = validation.split('.')
 
@@ -38,7 +39,53 @@ module.exports = class JoiPolicy extends Policy {
       }
 
       Joi.validate(req.body, Validator[schema](), (err, value) => {
-        if (err){
+        if(config.byPassError){
+          if(!_.isEmpty(err.details)){
+            let allPaths = err.details.map(e=>e.path.split('.'))
+            let uniquePaths = [], validationErrors=[]
+            allPaths.map((p,pindex)=>{
+              let pArr = p
+              pArr.pop()
+              if(!pindex){
+                uniquePaths.push(pArr)
+              }
+              else {
+                //console.log(`pArr `,pArr)
+                let diff = uniquePaths.findIndex(u=>{
+                  return _.isEmpty(_.differenceWith(pArr,u, _.isEqual))
+                })
+                if(diff===-1) uniquePaths.push(pArr)
+              }
+            })
+            uniquePaths.map(u=>{
+              validationErrors.push({
+                path:u.join('.'),
+                paths:u,
+                keys:[]
+              })
+            })
+            err.details.map(d=>{
+              d.paths = d.path.split('.')
+              d.paths.pop()
+              let index = validationErrors.findIndex(u=>{
+                return _.isEmpty(_.differenceWith(d.paths,u.paths, _.isEqual))
+              })
+              if(index>-1){
+                validationErrors[index].keys = validationErrors[index].keys.concat([d.context.key])
+              }
+            })
+            req.validationErrors = validationErrors
+            let newBody = Object.assign({},req.body)
+            validationErrors.map(d=>{
+              let keys = d.paths
+              let prop = keys.pop();
+              let parent =keys.reduce((n, key) => n[key], newBody);
+              delete parent[prop];
+            })
+            req.newBody = newBody
+          }
+        }
+        else{
           if (err.details && err.details[0].type == 'object.allowUnknown') {
             return res.status(400).json({ flag: false, data: {}, message: err.details[0].message, code: 400 });
           }
